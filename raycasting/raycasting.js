3142311f0811d6048b9b6dcfadb79ace5b00183b
Devpz <114014131+d3vpz@users.noreply.github.com>
2023-11-04 01:58:55 +0000

    first commit

---
+++ raycasting/raycasting.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',()=>{
+    const canvas=document.getElementById('screen');
+    const ctx=canvas.getContext('2d');
+
+    canvas.width=640;
+    canvas.height=480;
+});
