f05d78061065d2d7fc3dc4179a1ff77ac3348b9e
Devpz <114014131+d3vpz@users.noreply.github.com>
2023-11-04 23:20:58 +0000

    added favicon

--- raycasting/raycasting.js
+++ raycasting/raycasting.js
@@ -1,7 +1,38 @@
-window.addEventListener('load',()=>{
-    const canvas=document.getElementById('screen');
-    const ctx=canvas.getContext('2d');
+const canvas=document.getElementById('screen');
+const ctx=canvas.getContext('2d');
 
-    canvas.width=640;
-    canvas.height=480;
+canvas.width=640;
+canvas.height=480;
+
+let x=50;let y=50;
+
+window.addEventListener('load',()=>{
+    setInterval(loop,10);
+});
+
+function loop(){
+    fill_screen('#151515')
+    ctx.fillStyle='white';
+    ctx.fillRect(x-4,y-4,8,8);
+    if(keys['ArrowLeft']){x-=1;}
+    if(keys['ArrowRight']){x+=1;}
+    if(keys['ArrowUp']){y-=1;}
+    if(keys['ArrowDown']){y+=1;}
+}
+
+function blit(x,y){
+    ctx.fillRect(x,y,1,1);
+}
+
+function fill_screen(color){
+    ctx.fillStyle=color;
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+document.addEventListener('keydown',(e)=>{
+    keys[e.key]=1;
+});
+
+document.addEventListener('keyup',(e)=>{
+    keys[e.key]=0;
 });
